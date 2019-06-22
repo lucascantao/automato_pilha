1dbcfb7a706e09e94faa1b2527d8571c78bb7c9e
soldgear <soldgear@users.noreply.github.com>
2019-06-22 17:51:48 -0300

    variavel temporaria (campos) removida

--- automato.py
+++ automato.py
@@ -11,8 +11,6 @@ class Automato:
     estado_atual: str
     pilha = []
 
-    campos = [] # VARIAVEL TEMPORARIA PRA RECEBER A PRIMEIRA LINHA DO ARQUIVO
-
     modulos = []  # LISTA DE NOS DO AUTOMATO
 
     def __init__(self, gramatica: str):  # CONSTRUTOR DA CLASSSE RECEBENDO A GRAMATICA DO AUTOMATO (1° LINHA DO ARQUIVO)
@@ -22,19 +20,19 @@ class Automato:
         gramatica = gramatica.replace(')', '')
         gramatica = gramatica.replace('{', '|')
         gramatica = gramatica.replace('}', '|')
-        self.campos = gramatica.split('|')  # CAMPOS RECEBE UMA LISTA DOS CONJUNTOS DA GRAMATICA
+        gramatica = gramatica.split('|')  # CAMPOS RECEBE UMA LISTA DOS CONJUNTOS DA GRAMATICA
 
         # RETIRANDO ELEMENTOS INUTEIS DA LISTA
-        for i in self.campos:
+        for i in gramatica:
             if i == '' or i == ', ':  # TAIS COMO ISSO AÍ ;)
-                self.campos.remove(i)
+                gramatica.remove(i)
 
-        self.simbolos = self.campos[0].split(', ')   # DEFINIDO LISTA DE SÍMBOLOS
-        self.estados = self.campos[1].split(', ')   # DEFININDO LISTA DE ESTADOS
-        self.estados_finais = self.campos[3].split(', ')  # LISTA DE ESTADOS FINAIS
-        self.pilha = self.campos[4].split(', ')   # LISTA DE SIMBOLOS DE PILHA
+        self.simbolos = gramatica[0].split(', ')   # DEFINIDO LISTA DE SÍMBOLOS
+        self.estados = gramatica[1].split(', ')   # DEFININDO LISTA DE ESTADOS
+        self.estados_finais = gramatica[3].split(', ')  # LISTA DE ESTADOS FINAIS
+        self.pilha = gramatica[4].split(', ')   # LISTA DE SIMBOLOS DE PILHA
 
-        self.estado_incial = self.campos[2].split(', ')   # RESOLVENDO PROBLEMA DO ESTADO INCIAL JUNTO COM "D"
+        self.estado_incial = gramatica[2].split(', ')   # RESOLVENDO PROBLEMA DO ESTADO INCIAL JUNTO COM "D"
         self.estado_incial.remove('D')
         for i in self.estado_incial:
             if i == '':   # MAIS ELEMENTOS INUTEIS '-'
@@ -42,7 +40,7 @@ class Automato:
 
         self.estado_atual = self.estado_incial[0]  # O ESTADO ATUAL SERÁ IGUAL AO ESTADO INICIAL
 
-        print(self.campos)
+        print(gramatica)
 
         for i in self.estados:  # CRIANDO OS NOS DO AUTOMATO DE ACORDO COM OS ESTADOS E ADICIONANDO À LISTA DE MODULOS
             NO = no.No(i)   # PASSANDO O NOME DO NO PARA O CONSTRUOTR DA CLASSE
