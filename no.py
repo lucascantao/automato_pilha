c89a3186fe107b2c60d4abccd3568730eafe9d2d
soldgear <soldgear@users.noreply.github.com>
2019-06-22 12:35:43 -0300

    construtor atualizado

--- no.py
+++ no.py
@@ -2,10 +2,14 @@
 
 class No:
 
+    estado_origem = ""
     estado_destino = []
     simbolos = []
 
-    def __init__(self, dest, simb):
+    def __init__(self, estado_origem):
+        self.estado_origem = estado_origem
+
+    def setNo(self, dest, simb):
         self.estados_destino = dest[::]
         self.simbolos = simb[::]
 
